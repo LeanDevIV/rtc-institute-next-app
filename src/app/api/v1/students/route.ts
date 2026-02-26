95637c69a86821bbf8b733dcb5413333af061446
LeanDevIV <leandrocordoba318@gmail.com>
2026-02-26 07:55:08 -0300

    refactor: splitting bussiness logic from routers (dynamic route for students)

--- src/app/api/v1/students/route.ts
+++ src/app/api/v1/students/route.ts
@@ -1,71 +1,18 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
-import z from "zod";
+import { studentSchema } from "@/lib/schemas/Student"; // Add this import
+import { createStudent, getStudents } from "@/app/services/student.service";
+import { formatZodErrors } from "@/app/utils/errorHandler";
 //Get students
- export async function GET(request: Request) {
-   try {
-     const students = await prisma.student.findMany();
-     return NextResponse.json(
-       { message: "Estudiantes encontrados:", students },
-       { status: 200 },
-     );
-   } catch (error) {
-     console.error(error);
-     return NextResponse.json(
-       { message: "Error interno del servidor" },
-       { status: 500 },
-     );
-   }
- }
-//ZodSchema
-const studentSchema = z.object({
-  name: z
-    .string()
-    .trim()
-    .min(3, "El nombre debe tener al menos 3 caracteres")
-    .max(50, "El nombre debe tener menos de 50 caracteres"),
-  surname: z
-    .string()
-    .trim()
-    .min(3, "El apellido debe tener al menos 3 caracteres")
-    .max(50, "El apellido debe tener menos de 50 caracteres"),
-  email: z.email("Debe ingresar un correo válido"),
-  phone: z
-    .string()
-    .trim()
-    .min(10, "El teléfono debe tener al menos 10 caracteres")
-    .max(20, "El teléfono debe tener menos de 20 caracteres")
-    .optional(),
-});
-
-//Create student
-export async function POST(request: Request) {
+export async function GET(request: Request) {
   try {
-    const body = await request.json();
-    const result = studentSchema.safeParse(body);
-    if (!result.success) {
-      // const errors = z.treeifyError(result.error);
-      //crear Record de errores
-      const errors: Record<string, string> = {};
-      //recorrer errores
-      result.error.issues.forEach((issue) => {
-        //obtener campo
-        const field = issue.path[0] as string;
-        //agregar error
-        errors[field] = issue.message;
-      });
-      //retornar errores
-      return NextResponse.json(errors, { status: 400 });
-    }
-
-    const newStudent = await prisma.student.create({
-      data: result.data,
-    });
+    const students = await getStudents();
     return NextResponse.json(
-      { message: "Estudiante creado:", newStudent },
-      { status: 201 },
+      { message: "Estudiantes encontrados:", students },
+      { status: 200 },
     );
   } catch (error) {
+    //TODO: agregar logger para registrar errores y error handling más robusto
     console.error(error);
     return NextResponse.json(
       { message: "Error interno del servidor" },
@@ -74,5 +21,28 @@ export async function POST(request: Request) {
   }
 }
 
+//Create student
+export async function POST(request: Request) {
+  try {
+    //Solicitamos el cuerpo de la petición y lo parseamos como JSON para hacerlo más manejable
+    const body = await request.json();
 
+    //Validamos los datos de entrada usando Zod
+    const result = studentSchema.safeParse(body);
+    if (!result.success) {
+      // Si la validación falla, formateamos los errores y los devolvemos al cliente
+      const errors = formatZodErrors(result.error);
+      return NextResponse.json({ errors }, { status: 400 });
+    }
 
+    //Finalmente, si la validación es exitosa, creamos el estudiante en la base de datos
+    const student = await createStudent(result.data);
+    return NextResponse.json(student, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error interno del servidor" },
+      { status: 500 },
+    );
+  }
+}
